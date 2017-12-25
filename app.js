3c79a406ac26b46fd03aa194f55663217e919b61
Zachary J. Baldwin <zacharyjbaldwin@gmail.com>
2017-12-24 18:21:22 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// third-party modules
+var express = require('express');
+var session = require('express-session');
+
+// classes, constants, and user modules
+var MongoStore = require('connect-mongo')(session);
